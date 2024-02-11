ff72ee44a01e7958db8eaf38e2c78c3a35be2d81
Deji Martins <dejimartins99@gmail.com>
2024-02-12 00:34:46 +0100

    first commit

---
+++ src/main/java/com/coralpay/coralsee/CoralseeApplication.java
@@ -0,0 +1,13 @@
+package com.coralpay.coralsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoralseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoralseeApplication.class, args);
+	}
+
+}
